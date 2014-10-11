fe79841fa784c5a270a294b4f6caf3105ca47282
cjc <weiherty@gmail.com>
2014-10-11 15:30:00 +0800

    v1.2 使用后自杀

--- app/src/main/java/com/cf/cflock/MainActivity.java
+++ app/src/main/java/com/cf/cflock/MainActivity.java
@@ -18,22 +18,15 @@ public class MainActivity extends Activity {
     private DevicePolicyManager devicePolicyManager=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
         finish();
         devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
 
         if (devicePolicyManager.isAdminActive(Dar.getCn(this))) {
 
-           // finish();
 
             devicePolicyManager.lockNow();
-            try {
-                Thread.currentThread().sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
             android.os.Process.killProcess(android.os.Process.myPid());
-            //System.exit(0);//直接结束程序
         }else{
             startAddDeviceAdminAty();
         }
@@ -43,8 +36,6 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
 
-//或者下面这种方式
-//android.os.Process.killProcess(android.os.Process.myPid());
     }
     private void startAddDeviceAdminAty(){
         Intent i = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
